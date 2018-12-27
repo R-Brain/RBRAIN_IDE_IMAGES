82a04eb442f47478068af72372972c59af6efcbb
marami52 <marami@ironix.com>
2018-12-27 10:48:02 -0800

    add rsession config

--- RStudio/prepare_start.sh
+++ RStudio/prepare_start.sh
@@ -1,5 +1,7 @@
 
 #!/bin/bash
+mkdir -p /ride/libraries/r
+chown -R rstudio:rstudio /ride/libraries/r 
 sed -i -e "s/sessionId/${RBRAIN_SESSION_ID}\/ride/g" /etc/nginx/nginx.conf
 for line in $( cat /etc/environment ) ; do export $line ; done
 
