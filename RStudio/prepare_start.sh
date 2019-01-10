a5faf468814eace072b8240a919df196e6d155d7
Ali Marami <marami@ironix.com>
2019-01-10 13:24:15 -0800

    expose env variable for rstudio user

--- RStudio/prepare_start.sh
+++ RStudio/prepare_start.sh
@@ -1,12 +1,19 @@
-
 #!/bin/bash
 mkdir -p /ride/libraries/r
 mkdir -p /ride/work/projects
 chown -R rstudio:rstudio /ride/libraries/r 
 chown -R rstudio:rstudio /ride/work/projects
 sed -i -e "s/sessionId/${RBRAIN_SESSION_ID}\/ride/g" /etc/nginx/nginx.conf
+
+echo "USERNAME=${USERNAME}" >> /etc/environment 
+#All RBRAIN Env variables
+for line in ${!RBRAIN_*}; do echo $line=${!line} >> /etc/environment; done;
+# any variable stats with IEV_
+for line in ${!IEV_*}; do echo $line=${!line} >> /etc/environment; done;
+
 for line in $( cat /etc/environment ) ; do export $line ; done
 
 /etc/init.d/nginx start
 sleep 1s
+
 
