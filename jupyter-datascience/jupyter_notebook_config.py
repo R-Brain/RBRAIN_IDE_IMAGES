7aeabe8309f4ddf2a67cd1327e33186b873fe040
marami52 <marami@ironix.com>
2019-01-24 22:30:34 -0800

    add Jupyter image

---
+++ jupyter-datascience/jupyter_notebook_config.py
@@ -0,0 +1,85 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+from jupyter_core.paths import jupyter_data_dir
+import subprocess
+import os
+import errno
+import stat
+
+c = get_config()
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8080
+c.NotebookApp.open_browser = False
+c.NotebookApp.allow_origin = '*'
+c.NotebookApp.token=''
+c.NotebookApp.tornado_settings = {
+    'compress_response': 'True'
+  }
+
+if 'RBRAIN_WEBAPP_URL' in os.environ:
+  c.NotebookApp.tornado_settings = {
+    'compress_response': 'True',
+    'headers': {
+      'Content-Security-Policy': "frame-ancestors " + os.environ['RBRAIN_WEBAPP_URL'] + " 'self' "
+    }
+  }
+
+  # Set a certificate if USE_HTTPS is set to any value
+if 'USE_HTTPS' in os.environ:
+    if not os.path.isfile(PEM_FILE):
+        # Ensure PEM_FILE directory exists
+        dir_name = os.path.dirname(PEM_FILE)
+        try:
+            os.makedirs(dir_name)
+        except OSError as exc: # Python >2.5
+            if exc.errno == errno.EEXIST and os.path.isdir(dir_name):
+                pass
+            else: raise
+        # Generate a certificate if one doesn't exist on disk
+        subprocess.check_call(['openssl', 'req', '-new', 
+            '-newkey', 'rsa:2048', '-days', '365', '-nodes', '-x509',
+            '-subj', '/C=XX/ST=XX/L=XX/O=generated/CN=generated',
+            '-keyout', PEM_FILE, '-out', PEM_FILE])
+        # Restrict access to PEM_FILE
+        os.chmod(PEM_FILE, stat.S_IRUSR | stat.S_IWUSR)
+    c.NotebookApp.certfile = PEM_FILE
+
+
+
+
+# https://github.com/jupyter/notebook/issues/3130
+c.FileContentsManager.delete_to_trash = False
+
+# Generate a self-signed certificate
+if 'GEN_CERT' in os.environ:
+    dir_name = jupyter_data_dir()
+    pem_file = os.path.join(dir_name, 'notebook.pem')
+    try:
+        os.makedirs(dir_name)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(dir_name):
+            pass
+        else:
+            raise
+    # Generate a certificate if one doesn't exist on disk
+    subprocess.check_call(['openssl', 'req', '-new',
+                           '-newkey', 'rsa:2048',
+                           '-days', '365',
+                           '-nodes', '-x509',
+                           '-subj', '/C=XX/ST=XX/L=XX/O=generated/CN=generated',
+                           '-keyout', pem_file,
+                           '-out', pem_file])
+    # Restrict access to the file
+    os.chmod(pem_file, stat.S_IRUSR | stat.S_IWUSR)
+    c.NotebookApp.certfile = pem_file
+
+# Change default umask for all subprocesses of the notebook server if set in
+# the environment
+if 'NB_UMASK' in os.environ:
+    os.umask(int(os.environ['NB_UMASK'], 8))
+
+if 'RBRAIN_SESSION_ID' in os.environ:
+    rBrainSessionId = os.environ['RBRAIN_SESSION_ID']
+    c.NotebookApp.base_url = rBrainSessionId +'/ride'
+
