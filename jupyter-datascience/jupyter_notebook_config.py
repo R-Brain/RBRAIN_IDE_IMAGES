e9ebda798930e648f53b06e7f218c05e494407e4
marami52 <marami@ironix.com>
2019-02-24 10:34:03 -0800

    add nginx and supervisor

--- jupyter-datascience/jupyter_notebook_config.py
+++ jupyter-datascience/jupyter_notebook_config.py
@@ -9,7 +9,7 @@ import stat
 
 c = get_config()
 c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.port = 8080
+c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
 c.NotebookApp.allow_origin = '*'
 c.NotebookApp.token=''
